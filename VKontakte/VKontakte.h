7b4fcff0c0fa4d45dee1619a7131425569701819
Luka Mirosevic <luka@goonbee.com>
2013-09-09 14:23:18 +0200

    Initial Commit

---
+++ VKontakte/VKontakte.h
@@ -0,0 +1,13 @@
+//
+//  VKontakte.h
+//  VKontakte
+//
+//  Created by Luka Mirosevic on 09/09/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VKontakte : NSObject
+
+@end
---
+++ VKontakte/VKontakte.m
@@ -0,0 +1,13 @@
+//
+//  VKontakte.m
+//  VKontakte
+//
+//  Created by Luka Mirosevic on 09/09/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "VKontakte.h"
+
+@implementation VKontakte
+
+@end
